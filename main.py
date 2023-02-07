9872245ff8828e4af5dc2a2f8940f3668e9ebc56
YashGoswamiGOAT <124524109+YashGoswamiGOAT@users.noreply.github.com>
2023-02-07 13:51:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-open("yash.txt","w").write("Hello")
-
 @app.route("/")
 def index():
+    open("yash.txt","w").write("Hello")
     return "Hello Yash its working"
 
+@app.route("/file")
+def index():
+    return str(open("yash.txt","r").read())
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port= 5000)
