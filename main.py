a046f2894dffdcfb8fedd6feaa801412a9d1afe4
YashGoswamiGOAT <124524109+YashGoswamiGOAT@users.noreply.github.com>
2023-02-07 13:48:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+open("yash.txt","w").write("Hello")
+
 @app.route("/")
 def index():
     return "Hello Yash its working"
