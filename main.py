ffb208e5e80ca5d0e3edd5c448773e2af0d60fa6
YashGoswamiGOAT <124524109+YashGoswamiGOAT@users.noreply.github.com>
2023-02-07 13:57:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,103 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+import flask_cors
+from bs4 import BeautifulSoup
+from flask import Flask, send_from_directory, send_file
+from flask_cors import CORS
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import json
+from pytube import YouTube
 
-@app.route("/")
-def index():
-    open("yash.txt","w").write("Hello")
-    return "Hello Yash its working"
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate(
+{
+  "type": "service_account",
+  "project_id": "yashwebsitebalaji",
+  "private_key_id": "d030e1c3b3b2dea792cc451688b6d7fec5f32c29",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDqBaIfD3TvnIs1\nrW2pT1/IOGE2jrxHBysWovBf/hbaO2eXawrML1CdtQha++OYmTmQMc3JCWuDKGo6\n/8j/ve5H/fpdDyhxrl6hoL3+jAeXG0Jm/KPJ6oiIyhBrDiD/bqhes8r4wINaCPvc\nQGfpVScubPe8GLJtPHaa3Q4RuaUI+HAceK8QFIP0bd9fTwnIQhPneFt5XWZw8+j6\n023uXUKCosOD1DrkhnTKH+qF5NMNuxChY7GsBe4CU5y/hGFAT4+7Xu1UJrQJUo2z\nwrLJvWtoeXIISXuJJniaQRd1hJ7qSiEH/cdxL/dcVKNS6SoTzcCjiE/uN1tGCxwH\nUG9Ma7kzAgMBAAECggEAGtHpdMN/Oc9y5C8g5b9bMVBGJ+FaMjkKlvi2hrHRPv0s\nOp4XFIk0T9awWJ5h75pZOVgpMZSQS0LwUHGsa07Xm6LqX2GTgi/N2EUv8RfC7kOU\nARyMIuHA+GL8AwuRUY+GNqWb/z/aQzvLWEB+CC+XospJ5rcyil1cab7S1B+MCp0X\njAUkFApKxAAuLNkb5gly0w+T1SZQYqSGw17Jk5Za29CiokJqKqcwRiCdec4quyQs\n54G8/R2GlM0xLcMcB+LVqPj6QhAril9iZevqatpa7q5qpeV0uY/Us7XGRUm4cLDd\nMEE5bCB4mWKbXKUh3Q0xV+vQY69+N1nudYA/XAbzAQKBgQD99HR6ma7GhxP9MT4C\nZqdXj3zPvb55HvBHMb3p4H0OAFMPhyJeJnF3iP6mhkf1bZznhiDXIRUbakJQ3+sP\n6LPiaq4wqrk3lna1rApmIA7oSAWI6AmJTIIiKXUhLwyr6jGsmEXDcoegFXA7c9Nc\n6Ii+0Q06+vk6oCr+HMdOgXIeMwKBgQDr6BXVnJN29yBmflMsBy89O33b0S3/4XFu\ncm3ei8vRqMrhC088nPOVavR1Y5lqk0+h0Jbh69dguZR4Py8BUBpThvbxPaf86nZi\nIOW6SnVdva6ks+1OIfgf2oDxjXAIr8Kimt3XyX1xrhGx8dNwfPM/pZkX+l1VulaC\nPrjW1/75AQKBgB86f1RQL7DUDX42HvT2oJ9g0q1NHO4SAuQOVtc6tsYQ/iXLrUs8\nmPllDEN4AcNJbmD+Gm258LmFUJBXaLV1HSG7kp4DcHFIfGKMvF6glJS1vpB+UCXl\nFZ+Tz4Z8HafKTb92niWRoOHroPh+nsAvDBnC5UiacilmZsMG0O9zdZAHAoGBAJ7z\na/LYY0wfS0eECop59MxvT2hTU4k73/ApTfpLe3OzJa/orOUMY9vjiv+lsnNq+pbv\nMxrai+5yXKWA/S0HShXJtI3rm0sAt+96dw/Ep7wX2JrFWTnyDXt5ALTBNiHEO4LQ\nknWZH9r/DJE3fd4ncscJz1OmsEjZGvz7QhQQyY4BAoGASOGBXn4s5YjGXbEZtxbP\n/QZ3pDfUgZDk1YfV8obifhpvvE9k7d/cyN4zO8WzkL9+g6UDq6gP4f5Z9uv/zUgT\n7pZZa30QO+UHGDYoW7um4oSxqZmY8VQ8iN3yclo6TV+349Me4CtGoqT5FUHC7ZWH\ndwj4DpfAu+jVcvNwhjLg0No=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-76bl1@yashwebsitebalaji.iam.gserviceaccount.com",
+  "client_id": "108511309136263506605",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-76bl1%40yashwebsitebalaji.iam.gserviceaccount.com"
+}
 
-@app.route("/file")
-def index():
-    return str(open("yash.txt","r").read())
+)
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://yashwebsitebalaji-default-rtdb.firebaseio.com/'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference()
+print(ref.get())
+
+app = Flask(__name__,static_folder="Uploads")
+CORS(app)
+
+@app.route("/webservice/universal")
+def universal():
+    web = ref.child("Website").child("universalVARR").get()
+    return web
+
+@app.route("/webservice/notifications")
+def notifications():
+    web = ref.child("Website").child("notifications").get()
+    return web
+
+@app.route("/webservice/blogs")
+def Blogs():
+    web = ref.child("Blogs").get()
+    return str(web).replace("'",'"')
+
+@app.route("/getBlog/<id>")
+def GetBlog(id):
+    web = json.dumps(ref.child("Blog").child(id).get())
+    return str(web)
+
+@app.route("/getMusic/<name>")
+def music(name):
+    txt = requests.get("https://www.youtube.com/results?search_query=" + name).text
+    soup = BeautifulSoup(txt, "lxml")
+
+    youtubedataobj = soup.find_all("script")[33].text.split("var ytInitialData = ")[1].split("};")[0] + "}"
+    raw = (
+    json.loads(youtubedataobj)["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"][
+        "contents"][0]["itemSectionRenderer"]["contents"])
+    videos = []
+    # v[1].videoRenderer.navigationEndpoint.commandMetadata.webCommandMetadata.url
+    for i in range(len(raw)):
+        try:
+            # v[4].videoRenderer.title.runs[0].text ---title
+            # v[4].videoRenderer.thumbnail.thumbnails[v[4].videoRenderer.thumbnail.thumbnails.length-1] -- thumbnail
+            videos.append(
+                {
+                    "title": raw[i]["videoRenderer"]["title"]["runs"][0]["text"],
+                    "thumbnail": raw[i]["videoRenderer"]["thumbnail"]["thumbnails"][
+                        len(raw[i]["videoRenderer"]["thumbnail"]["thumbnails"]) - 1],
+                    "video": raw[i]["videoRenderer"]["navigationEndpoint"]["commandMetadata"]["webCommandMetadata"][
+                        "url"]
+                }
+            )
+        except:
+            pass
+
+    return str(json.dumps(videos))
+
+@app.route("/download/<url>")
+def downloadMusic(url):
+    YoutubeObject = YouTube("https://music.youtube.com/watch?v="+url)
+    dd = YoutubeObject.streams.get_audio_only().download("Uploads")
+    return send_file(dd.title())
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port= 5000)
+
+
+# C:\Users\Yash Goswami\Desktop\WebBio\OFFCIAL_WEB\contents\OFF\back\Uploads\BTS (방탄소년단) FAKE LOVE Official MV.mp4
+# C:\Users\Yash Goswami\Desktop\WebBio\OFFCIAL_WEB\contents\OFF\back\Uploads\BTS (방탄소년단) 'FAKE LOVE' Official MV.mp4
